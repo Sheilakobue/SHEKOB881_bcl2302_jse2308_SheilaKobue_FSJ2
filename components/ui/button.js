981c8bcbdca5f79193e40d620331fa0580c963c5
Sheila Kobue <maserole.mojela@gmail.com>
2023-09-11 09:06:56 +0200

    commended button and error component

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,8 +1,20 @@
-import Link from 'next/link';
+/**
+ * Button Component
+ *
+ * This component renders a button or a link based on the provided props.
+ *
+ * @param {object} props - The properties for the Button component.
+ * @param {string} [props.link] - The URL to link to (optional).
+ * @param {function} [props.onClick] - The click handler function (optional).
+ * @param {ReactNode} props.children - The content to be displayed inside the button.
+ * @returns {ReactNode} - The rendered button or link element.
+ */
+import Link from "next/link";
 
-import classes from './button.module.css';
+import classes from "./button.module.css";
 
-function Button(props) {
+export default function Button(props) {
+  // If a link is provided, render a Link component.
   if (props.link) {
     return (
       <Link href={props.link} className={classes.btn}>
@@ -11,6 +23,7 @@ function Button(props) {
     );
   }
 
+  // If no link is provided, render a button element with an optional onClick handler.
   return (
     <button className={classes.btn} onClick={props.onClick}>
       {props.children}
@@ -18,4 +31,4 @@ function Button(props) {
   );
 }
 
-export default Button;
+
--- components/ui/error-alert.js
+++ components/ui/error-alert.js
@@ -1,7 +1,22 @@
-import classes from './error-alert.module.css';
+/**
+ * Import the CSS module for styling the ErrorAlert component.
+ * @module './error-alert.module.css'
+ */
+import classes from "./error-alert.module.css";
 
-function ErrorAlert(props) {
+/**
+ * Represents an ErrorAlert component that displays an error message.
+ * @function ErrorAlert
+ * @param {Object} props - The properties passed to the component.
+ * @param {ReactNode} props.children - The child elements to be displayed inside the ErrorAlert.
+ * @returns {ReactElement} A React component that displays an error message.
+ */
+export default function ErrorAlert(props) {
+  /**
+   * Render an error alert div with the specified CSS class.
+   * @returns {ReactElement} A div element styled as an error alert.
+   */
   return <div className={classes.alert}>{props.children}</div>;
 }
 
-export default ErrorAlert;
+
