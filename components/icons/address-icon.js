49b3b8bfa6ca76c801800918c9711bc661bb22ff
Sheila Kobue <maserole.mojela@gmail.com>
2023-09-11 09:19:16 +0200

    added comments to the icon components

--- components/icons/address-icon.js
+++ components/icons/address-icon.js
@@ -1,25 +1,33 @@
+/**
+ * Represents an AddressIcon component that renders an SVG icon.
+ *
+ * @returns {JSX.Element} JSX representing the AddressIcon component.
+ */
 function AddressIcon() {
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
     >
+      {/* Path for the outline of the address icon */}
       <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
+        strokeLinecap="round"
+        strokeLinejoin="round"
         strokeWidth={2}
-        d='M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z'
+        d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
       />
+      {/* Path for a part of the address icon */}
       <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
+        strokeLinecap="round"
+        strokeLinejoin="round"
         strokeWidth={2}
-        d='M15 11a3 3 0 11-6 0 3 3 0 016 0z'
+        d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
       />
     </svg>
   );
 }
 
+// Export the AddressIcon component as the default export
 export default AddressIcon;
