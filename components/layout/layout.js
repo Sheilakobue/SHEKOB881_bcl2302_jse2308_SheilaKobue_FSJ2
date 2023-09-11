0337d83767898ec6ad55a149fde2e16ee0c30bf6
Sheila Kobue <maserole.mojela@gmail.com>
2023-09-11 09:10:14 +0200

    added comments on the layout,main-header

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,14 +1,22 @@
-import { Fragment } from 'react';
+/**
+ * Represents the Layout component of the application.
+ * This component defines the overall layout structure with a header and main content area.
+ *
+ * @param {object} props - The properties passed to the Layout component.
+ * @param {ReactNode} props.children - The child elements/content to be rendered within the main content area.
+ * @returns {JSX.Element} - A JSX element representing the layout structure.
+ */
+import { Fragment } from "react";
+import MainHeader from "./main-header";
 
-import MainHeader from './main-header';
-
-function Layout(props) {
+export default function Layout(props) {
   return (
     <Fragment>
+      {/* Render the MainHeader component as the header of the layout */}
       <MainHeader />
       <main>{props.children}</main>
     </Fragment>
   );
 }
 
-export default Layout;
+
--- components/layout/main-header.js
+++ components/layout/main-header.js
@@ -1,17 +1,25 @@
-import Link from 'next/link';
+import Link from "next/link"; // Import the Link component from the 'next/link' module.
 
-import classes from './main-header.module.css';
+import classes from "./main-header.module.css"; // Import styles from a CSS module.
 
+/**
+ * Functional component representing the main header of a web page.
+ * This header typically contains a logo and navigation links.
+ *
+ * @returns {JSX.Element} JSX element representing the main header.
+ */
 function MainHeader() {
   return (
     <header className={classes.header}>
       <div className={classes.logo}>
-        <Link href='/'>NextEvents</Link>
+        {/* Create a navigation link to the homepage */}
+        <Link href="/">NextEvents</Link>
       </div>
       <nav className={classes.navigation}>
         <ul>
           <li>
-            <Link href='/events'>Browse All Events</Link>
+            {/* Create a navigation link to the 'events' page */}
+            <Link href="/events">Browse All Events</Link>
           </li>
         </ul>
       </nav>
@@ -19,4 +27,4 @@ function MainHeader() {
   );
 }
 
-export default MainHeader;
+export default MainHeader; // Export the MainHeader component as the default export.
