8469188e515dd80f026d700b53e86b501a01e9fd
Sheila Kobue <maserole.mojela@gmail.com>
2023-09-11 09:00:41 +0200

    comment on eventId component

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,18 +1,30 @@
-import { Fragment } from 'react';
-import { useRouter } from 'next/router';
+/**
+ * This is the EventDetailPage component, responsible for displaying details of a specific event.
+ * @returns {JSX.Element} The JSX element representing the event detail page.
+ */
+import { Fragment } from "react";
+import { useRouter } from "next/router";
 
-import { getEventById } from '../../dummy-data';
-import EventSummary from '../../components/event-detail/event-summary';
-import EventLogistics from '../../components/event-detail/event-logistics';
-import EventContent from '../../components/event-detail/event-content';
-import ErrorAlert from '../../components/ui/error-alert';
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/error-alert";
 
-function EventDetailPage() {
+/**
+ * Function representing the EventDetailPage component.
+ * @returns {JSX.Element} The JSX element representing the event detail page.
+ */
+export default function EventDetailPage() {
   const router = useRouter();
 
+  // Extract the eventId from the router's query parameters.
   const eventId = router.query.eventId;
+
+  // Fetch event details using the eventId.
   const event = getEventById(eventId);
 
+  // If no event is found, display an error message.
   if (!event) {
     return (
       <ErrorAlert>
@@ -21,6 +33,7 @@ function EventDetailPage() {
     );
   }
 
+  // Render event details including summary, logistics, and content.
   return (
     <Fragment>
       <EventSummary title={event.title} />
@@ -37,4 +50,3 @@ function EventDetailPage() {
   );
 }
 
-export default EventDetailPage;
