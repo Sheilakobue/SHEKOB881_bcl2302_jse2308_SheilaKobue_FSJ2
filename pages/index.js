588bf77be10a089aee2562e816567a9705d77301
Sheila Kobue <maserole.mojela@gmail.com>
2023-09-11 08:10:09 +0200

    commented the index code

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
-import { getFeaturedEvents } from '../dummy-data';
-import EventList from '../components/events/event-list';
+/**
+ * This module represents the homepage of the application.
+ * @module HomePage
+ */
 
-function HomePage() {
+import { getFeaturedEvents } from "../dummy-data"; // Import the function to fetch featured events data.
+import EventList from "../components/events/event-list"; // Import the EventList component.
+
+/**
+ * Represents the homepage of the application.
+ * @function
+ * @returns {JSX.Element} JSX representing the homepage content.
+ */
+export default function HomePage() {
+  // Fetch the featured events data using the getFeaturedEvents function.
   const featuredEvents = getFeaturedEvents();
 
+  // Return JSX representing the homepage content.
   return (
     <div>
+      {/* Render the EventList component and pass the featured events data as items prop. */}
       <EventList items={featuredEvents} />
     </div>
   );
 }
 
-export default HomePage;
+
